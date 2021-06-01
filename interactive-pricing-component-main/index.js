d16166d0f4bd8355f0f5f60f618bbb6bf081bc90
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-01 08:17:01 -0300

    js improvments

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,115 +1,16 @@
-/* const pageviews = document.getElementById("pageviews");
-const desktopPrice = document.getElementById("desktop-price");
-const mobilePrice = document.getElementById("mobile-price");
-const toggle = document.getElementById("toggle");
-const inputbar = document.getElementById("myInput");
-
-toggle.oninput = function (e) {
-  if (e.target.checked) {
-    if (inputbar.value == 1) {
-      desktopPrice.textContent = "$6.00";
-      mobilePrice.textContent = "$6.00";
-    } else if (inputbar.value > 1 && inputbar.value < 32) {
-      desktopPrice.textContent = "$9.00";
-      mobilePrice.textContent = "$9.00";
-    } else if (inputbar.value >= 32 && inputbar.value < 65) {
-      desktopPrice.textContent = "$12.00";
-      mobilePrice.textContent = "$12.00";
-    } else if (inputbar.value >= 66 && inputbar.value < 99) {
-      desktopPrice.textContent = "$18.00";
-      mobilePrice.textContent = "$18.00";
-    } else if (inputbar.value == 100) {
-      desktopPrice.textContent = "$27.00";
-      mobilePrice.textContent = "$27.00";
-    }
-  } else {
-    if (inputbar.value == 1) {
-      desktopPrice.textContent = "$8.00";
-      mobilePrice.textContent = "$8.00";
-    } else if (inputbar.value > 1 && inputbar.value < 32) {
-      desktopPrice.textContent = "$12.00";
-      mobilePrice.textContent = "$12.00";
-    } else if (inputbar.value >= 32 && inputbar.value < 65) {
-      desktopPrice.textContent = "$16.00";
-      mobilePrice.textContent = "$16.00";
-    } else if (inputbar.value >= 66 && inputbar.value < 99) {
-      desktopPrice.textContent = "$24.00";
-      mobilePrice.textContent = "$24.00";
-    } else if (inputbar.value == 100) {
-      desktopPrice.textContent = "$36.00";
-      mobilePrice.textContent = "$36.00";
-    }
-  }
-};
-
-inputbar.oninput = function (e) {
-  if (e.target.value == 1) {
-    pageviews.textContent = "10k pageviews";
-    if (toggle.checked == true) {
-      desktopPrice.textContent = "$6.00";
-      mobilePrice.textContent = "$6.00";
-    } else {
-      desktopPrice.textContent = "$8.00";
-      mobilePrice.textContent = "$8.00";
-    }
-  } else if (e.target.value > 1 && e.target.value < 32) {
-    pageviews.textContent = "50k pageviews";
-    if (toggle.checked == true) {
-      desktopPrice.textContent = "$9.00";
-      mobilePrice.textContent = "$9.00";
-    } else {
-      desktopPrice.textContent = "$12.00";
-      mobilePrice.textContent = "$12.00";
-    }
-  } else if (e.target.value >= 32 && e.target.value < 65) {
-    pageviews.textContent = "100k pageviews";
-    if (toggle.checked == true) {
-      desktopPrice.textContent = "$12.00";
-      mobilePrice.textContent = "$12.00";
-    } else {
-      desktopPrice.textContent = "$16.00";
-      mobilePrice.textContent = "$16.00";
-    }
-  } else if (e.target.value >= 66 && e.target.value <= 99) {
-    pageviews.textContent = "500k pageviews";
-    if (toggle.checked == true) {
-      desktopPrice.textContent = "$18.00";
-      mobilePrice.textContent = "$18.00";
-    } else {
-      desktopPrice.textContent = "$24.00";
-      mobilePrice.textContent = "$24.00";
-    }
-  } else if (e.target.value == 100) {
-    pageviews.textContent = "1m pageviews";
-    if (toggle.checked == true) {
-      desktopPrice.textContent = "$27.00";
-      mobilePrice.textContent = "$27.00";
-    } else {
-      desktopPrice.textContent = "$36.00";
-      mobilePrice.textContent = "$36.00";
-    }
-  }
-  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
-  this.style.background =
-    "linear-gradient(to right, #10d5c2 0%, #10d5c2 " +
-    value +
-    "%, #eaeefb " +
-    value +
-    "%, #eaeefb 100.5%)";
-}; */
-
 /* Query Selector */
 
 const pageviews = document.querySelector("#pageviews span"),
-  price = document.querySelector(".price span"),
+  price = document.querySelectorAll(".price span"),
   toggle = document.querySelector(".toggle-container input"),
   inputbar = document.getElementById("myInput");
 
 // generate price on switch
-toggle.onclick = function () {
-  if (toggle.checked == true) {
-    let priceInt = parseInt(price.textContent);
-    price.textContent = `${priceInt - priceInt * 0.25}`;
+toggle.onclick = function (e) {
+  if (e.target.checked) {
+    let priceInt = parseInt(price[0].textContent);
+    price[0].textContent = `${priceInt - priceInt * 0.25}`;
+    price[1].textContent = `${priceInt - priceInt * 0.25}`;
   } else {
     refreshPrices();
   }
@@ -121,18 +22,22 @@ inputbar.addEventListener("input", refreshPrices);
 function refreshPrices() {
   if (inputbar.value == 1) {
     if (toggle.checked == true) {
-      price.textContent = `${8 - 8 * 0.25}`;
+      price[0].textContent = `${8 - 8 * 0.25}`;
+      price[1].textContent = `${8 - 8 * 0.25}`;
     } else {
-      price.textContent = 8;
+      price[0].textContent = 8;
+      price[1].textContent = 8;
     }
     pageviews.textContent = "10k";
   }
 
   if (inputbar.value == 2) {
     if (toggle.checked == true) {
-      price.textContent = 9;
+      price[0].textContent = 9;
+      price[1].textContent = 9;
     } else {
-      price.textContent = 12;
+      price[0].textContent = 12;
+      price[1].textContent = 12;
     }
 
     pageviews.textContent = "50k";
@@ -140,31 +45,34 @@ function refreshPrices() {
 
   if (inputbar.value == 3) {
     if (toggle.checked == true) {
-      price.textContent = 12;
+      price[0].textContent = 12;
+      price[1].textContent = 12;
     } else {
-      price.textContent = 16;
+      price[0].textContent = 16;
+      price[1].textContent = 16;
     }
-
     pageviews.textContent = "100k";
   }
 
   if (inputbar.value == 4) {
     if (toggle.checked == true) {
-      price.textContent = 18;
+      price[0].textContent = 18;
+      price[1].textContent = 18;
     } else {
-      price.textContent = 24;
+      price[0].textContent = 24;
+      price[1].textContent = 24;
     }
-
     pageviews.textContent = "500k";
   }
 
   if (inputbar.value == 5) {
     if (toggle.checked == true) {
-      price.textContent = 27;
+      price[0].textContent = 27;
+      price[1].textContent = 27;
     } else {
-      price.textContent = 36;
+      price[0].textContent = 36;
+      price[1].textContent = 36;
     }
-
     pageviews.textContent = "1M";
   }
 }
