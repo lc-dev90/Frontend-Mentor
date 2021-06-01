69482f020589b3ffd7ff8a00ea6aeb1c3bd59a79
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-01 07:57:02 -0300

    javascript algorithim updated

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,4 +1,4 @@
-const pageviews = document.getElementById("pageviews");
+/* const pageviews = document.getElementById("pageviews");
 const desktopPrice = document.getElementById("desktop-price");
 const mobilePrice = document.getElementById("mobile-price");
 const toggle = document.getElementById("toggle");
@@ -89,6 +89,88 @@ inputbar.oninput = function (e) {
       mobilePrice.textContent = "$36.00";
     }
   }
+  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #10d5c2 0%, #10d5c2 " +
+    value +
+    "%, #eaeefb " +
+    value +
+    "%, #eaeefb 100.5%)";
+}; */
+
+/* Query Selector */
+
+const pageviews = document.querySelector("#pageviews span"),
+  price = document.querySelector(".price span"),
+  toggle = document.querySelector(".toggle-container input"),
+  inputbar = document.getElementById("myInput");
+
+// generate price on switch
+toggle.onclick = function () {
+  if (toggle.checked == true) {
+    let priceInt = parseInt(price.textContent);
+    price.textContent = `${priceInt - priceInt * 0.25}`;
+  } else {
+    refreshPrices();
+  }
+};
+
+// generate price on range
+inputbar.addEventListener("input", refreshPrices);
+
+function refreshPrices() {
+  if (inputbar.value == 1) {
+    if (toggle.checked == true) {
+      price.textContent = `${8 - 8 * 0.25}`;
+    } else {
+      price.textContent = 8;
+    }
+    pageviews.textContent = "10k";
+  }
+
+  if (inputbar.value == 2) {
+    if (toggle.checked == true) {
+      price.textContent = 9;
+    } else {
+      price.textContent = 12;
+    }
+
+    pageviews.textContent = "50k";
+  }
+
+  if (inputbar.value == 3) {
+    if (toggle.checked == true) {
+      price.textContent = 12;
+    } else {
+      price.textContent = 16;
+    }
+
+    pageviews.textContent = "100k";
+  }
+
+  if (inputbar.value == 4) {
+    if (toggle.checked == true) {
+      price.textContent = 18;
+    } else {
+      price.textContent = 24;
+    }
+
+    pageviews.textContent = "500k";
+  }
+
+  if (inputbar.value == 5) {
+    if (toggle.checked == true) {
+      price.textContent = 27;
+    } else {
+      price.textContent = 36;
+    }
+
+    pageviews.textContent = "1M";
+  }
+}
+
+// bar
+inputbar.oninput = function (e) {
   var value = ((this.value - this.min) / (this.max - this.min)) * 100;
   this.style.background =
     "linear-gradient(to right, #10d5c2 0%, #10d5c2 " +
