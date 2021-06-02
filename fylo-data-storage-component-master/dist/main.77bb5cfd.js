e738823ee06f4741a54c3c298563bf0a2c4fe399
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-02 15:29:37 -0300

    mobile

--- fylo-data-storage-component-master/dist/main.77bb5cfd.js
+++ fylo-data-storage-component-master/dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../images/bg-desktop.png":[["bg-desktop.f8e799e4.png","images/bg-desktop.png"],"images/bg-desktop.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../images/bg-desktop.png":[["bg-desktop.f8e799e4.png","images/bg-desktop.png"],"images/bg-desktop.png"],"./../images/bg-mobile.png":[["bg-mobile.25f8a1c9.png","images/bg-mobile.png"],"images/bg-mobile.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
