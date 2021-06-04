27f19f11242342a6bdb6cf04d564d1b0967a963e
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-04 03:23:23 -0300

    project finished

--- loopstudios-landing-page-main/index.js
+++ loopstudios-landing-page-main/index.js
@@ -1,25 +1,32 @@
 const imgGuyElement = document.querySelector("article div img");
 const cards = document.querySelectorAll(".card");
+const box = document.querySelector(".box");
+const btn = box.querySelector(".btn");
+const navmobile = document.querySelector(".nav-mobile");
 
 window.addEventListener("resize", () => {
-  console.log(window.innerWidth);
   imgGuyElement.setAttribute(
     "src",
     `./images/${
       window.innerWidth < 980 ? "mobile" : "desktop"
     }/image-interactive.jpg`
   );
+  if (window.innerWidth > 768) {
+    document.querySelector("header").classList.remove("height");
+    document.body.classList.remove("height");
+    navmobile.classList.remove("open");
+    btn.classList.remove("active");
+  }
 });
 
-const box = document.querySelector(".box");
 box.onclick = function () {
-  const btn = box.querySelector(".btn");
-  btn.classList.toggle("active");
-  btn.classList.toggle("not-active");
+  if (btn.classList.contains("active")) {
+    btn.className = "not-active";
+  } else {
+    btn.className = "active";
+  }
 };
-
 box.addEventListener("click", function (e) {
-  const navmobile = document.querySelector(".nav-mobile");
   navmobile.classList.toggle("open");
   var opacity = 0;
   function MyFadeFunction() {
@@ -33,4 +40,18 @@ box.addEventListener("click", function (e) {
     document.querySelector("nav .logo").style.opacity = opacity;
   }
   MyFadeFunction();
+  document.querySelector("header").classList.toggle("height");
+  document.body.classList.toggle("height");
 });
+
+var i = 0;
+var txt = "Immersive experiences that deliver";
+var speed = 100;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+document.addEventListener("load", typeWriter());
