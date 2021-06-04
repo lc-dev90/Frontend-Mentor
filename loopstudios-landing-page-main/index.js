97375a8025a3a84cce6bd7499e78fca6c19d031d
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-03 21:48:03 -0300

    hamburguer menu

--- loopstudios-landing-page-main/index.js
+++ loopstudios-landing-page-main/index.js
@@ -2,10 +2,23 @@ const imgGuyElement = document.querySelector("article div img");
 
 window.addEventListener("resize", () => {
   console.log(window.innerWidth);
-  /*   imgGuyElement.setAttribute(
+  imgGuyElement.setAttribute(
     "src",
     `./images/${
       window.innerWidth < 980 ? "mobile" : "desktop"
-    }/image-interactive.jpg` 
-  );*/
+    }/image-interactive.jpg`
+  );
 });
+
+const box = document.querySelector(".box");
+box.onclick = function () {
+  const btn = box.querySelector(".btn");
+  btn.classList.toggle("active");
+  btn.classList.toggle("not-active");
+}; /* 
+box.addEventListener("click", function (e) {
+  const btn = box.querySelector(".btn");
+  btn.classList.toggle("active");
+  btn.classList.toggle("not-active");
+});
+ */
