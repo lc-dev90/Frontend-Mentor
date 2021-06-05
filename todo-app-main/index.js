e4b5b46c72693bafdad10e14780946cd78d127a4
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-05 01:53:29 -0300

    make dragabble and editabble

--- todo-app-main/index.js
+++ todo-app-main/index.js
@@ -9,11 +9,17 @@ function insertTodo(e) {
   e.preventDefault();
   const todoText = todoInput.value;
   const todoItem = document.createElement("li");
+  todoItem.setAttribute("ondblclick", "test(this)");
   todoItem.className = "todo";
+  const check = document.createElement("div");
+  check.innerHTML = `<i class="fas fa-check-circle"></i>`;
+  check.className = "check";
   const paragraph = document.createElement("p");
+  paragraph.className = "todo-text";
   paragraph.innerText = todoText;
   const span = document.createElement("span");
   span.innerHTML = `<i class="fas fa-times-circle"></i>`;
+  todoItem.appendChild(check);
   todoItem.appendChild(paragraph);
   span.className = "close";
   todoItem.appendChild(span);
@@ -21,17 +27,34 @@ function insertTodo(e) {
 }
 
 document.addEventListener("click", function (e) {
-  console.log(e.target);
   if (e.target.classList.contains("close")) {
     e.target.parentElement.remove();
   }
-  if (e.target.classList.contains("todo")) {
-    /* e.target.childNodes.classList.toggle("completed"); */
-    e.target.children[0].classList.toggle("completed");
+  if (e.target.classList.contains("check")) {
     e.target.classList.toggle("completed");
+    console.log(e.target.parentElement);
+    e.target.parentElement.querySelector("p").classList.toggle("completed");
+    /* e.target.childNodes[1].classList.toggle("completed"); */
+  }
+  if (e.target.classList.contains("todo")) {
+    var todoText = e.target.querySelector(".todo-text");
+    todoText.contentEditable = true;
   }
 });
 const dragBox = document.querySelector(".dragbox");
 new Sortable(dragBox, {
   animation: 400,
 });
+
+function test(e) {
+  const that = e;
+  console.log(that);
+  e.classList.add("editable");
+  e.querySelector(".todo-text").contentEditable = true;
+  e.querySelector(".todo-text").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      e.target.contentEditable = false;
+      that.classList.remove("editable");
+    }
+  });
+}
