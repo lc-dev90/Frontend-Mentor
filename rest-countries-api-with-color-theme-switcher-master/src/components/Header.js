d989fef9f07c5d0e4c75e838cb92093cef8be204
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-24 12:08:50 -0300

    header

--- rest-countries-api-with-color-theme-switcher-master/src/components/Header.js
+++ rest-countries-api-with-color-theme-switcher-master/src/components/Header.js
@@ -4,14 +4,43 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <Container>
-      <h1>Where in the world?</h1>
+      <nav>
+        <h1>Where in the world?</h1>
+
+        <div>
+          <a href="/">
+            <i className="fas fa-moon"></i>Dark Mode
+          </a>
+        </div>
+      </nav>
     </Container>
   );
 };
 
 export default Header;
 
-const Container = styled.nav`
+const Container = styled.div`
   height: 80px;
+  width: 100vw;
   background-color: #283640;
+  color: white;
+  nav {
+    width: 1280px;
+    max-width: 95%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    div {
+      display: flex;
+      a {
+        color: white;
+        font-weight: 600;
+        i {
+          margin-right: 6px;
+        }
+      }
+    }
+  }
 `;
