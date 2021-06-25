3f3fb7f39d2fdc7c45cb30631c9598cbffb877ec
lc-dev90 <lc.danella.dev@gmail.com>
2021-06-25 13:08:22 -0300

    implemented not found router

--- rest-countries-api-with-color-theme-switcher-master/src/App.js
+++ rest-countries-api-with-color-theme-switcher-master/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Details from "./components/Details";
 import Home from "./components/Home";
+import NotFoundPage from "./components/NotFoundPage";
 import styled from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SearchProvider } from "./components/SearchContext";
@@ -8,19 +9,20 @@ import { SelectProvider } from "./components/SelectContext";
 
 function App() {
   return (
-    <Router>
-      <SearchProvider>
-        <SelectProvider>
+    <SearchProvider>
+      <SelectProvider>
+        <Router>
           <Container>
             <Header />
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/details" exact component={Details} />
+              <Route component={NotFoundPage} />
             </Switch>
           </Container>
-        </SelectProvider>
-      </SearchProvider>
-    </Router>
+        </Router>
+      </SelectProvider>
+    </SearchProvider>
   );
 }
 
---
+++ rest-countries-api-with-color-theme-switcher-master/src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>NOT FOUND 404</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
